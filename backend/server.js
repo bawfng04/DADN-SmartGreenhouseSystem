8ae9cc6c4ab1c8bae1cbd2effea9a1a57abe6378
bawfng04 <nguyendinhbang53az@gmail.com>
2025-03-02 16:08:00 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,7 @@ const app = express();
 
 app.use(express.json());
 
-const corsOptions ={
-   origin:'*',
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
+app.use(cors());
 
 // use routes
 app.use("/api", exampleRoute);
