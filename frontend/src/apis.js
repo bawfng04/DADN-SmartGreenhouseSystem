cb4b382d8352c4b1dc7e27a4d45e590bd902d582
bawfng04 <nguyendinhbang53az@gmail.com>
2025-03-02 16:08:11 +0700

    .

--- frontend/src/apis.js
+++ frontend/src/apis.js
@@ -1,11 +1,11 @@
 // const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 // run on local, run normally
-export const REACT_APP_API_URL = "http://localhost:8000/api";
+// export const REACT_APP_API_URL = "http://localhost:8000/api";
 
 // run on the deployed server URL, blocked by CORS policy
-// export const REACT_APP_API_URL =
-//   "https://dadn-cjo832qjq-bawfng04s-projects.vercel.app/api";
+export const REACT_APP_API_URL =
+  "https://dadn-2.onrender.com/api";
 
 
 export const exampleAPI = `${REACT_APP_API_URL}/example`;
