4c4a8498aba9bc5685ce2e4db4fce3f3694030f3
bawfng04 <nguyendinhbang53az@gmail.com>
2025-03-02 15:41:41 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -16,22 +16,13 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://dadn-gamma.vercel.app",
-];
+const corsOptions ={
+   origin:'*',
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
 // use routes
