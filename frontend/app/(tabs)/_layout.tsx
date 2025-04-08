fdd1963da8bc5e848c723a2ee369926e1b43ba85
NhuNguyen0109 <nhu.nguyenquynh0109@hcmut.edu.vn>
2025-04-09 01:07:10 +0700

    add navbar

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,68 +1,126 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, View, StyleSheet, Dimensions } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
+import { useBottomTabOverflow } from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import HomeIcon from "@/assets/icons/home-28.svg";
+import DashboardIcon from "@/assets/icons/dashboard-28.svg";
+import SettingIcon from "@/assets/icons/setting-28.svg";
+import ReminderIcon from "@/assets/icons/reminder-28.svg";
+import ProfileIcon from "@/assets/icons/profile-28.svg";
+import HomeIconFocused from "@/assets/icons/home-focus-44.svg";
+import DashboardIconFocused from "@/assets/icons/dashboard-focus-44.svg";
+import ReminderIconFocused from "@/assets/icons/reminder-focus-44.svg";
+import SettingIconFocused from "@/assets/icons/setting-focus-44.svg";
+import ProfileIconFocused from "@/assets/icons/profile-focus-44.svg";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: "absolute",
-          },
-          default: {},
-        }),
+        // tabBarButton: HapticTab,
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          opacity: 1,
+          backgroundColor: "transparent",
+        },
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 20,
+          left: 20,
+          right: 20,
+          backgroundColor: "#00712D",
+          borderRadius: 30,
+          height: 68,
+          marginHorizontal: 20,
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+          flexDirection: "row",
+          paddingBottom: 28,
+          ...styles.shadow,
+        },
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
       <Tabs.Screen
         name="dashboard"
         options={{
           title: "Dashboard",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <DashboardIconFocused width={44} height={44} color={color} />
+            ) : (
+              <DashboardIcon width={28} height={28} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="reminder"
+        options={{
+          title: "Reminder",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <ReminderIconFocused width={44} height={44} color={color} />
+            ) : (
+              <ReminderIcon width={28} height={28} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <HomeIconFocused width={44} height={44} color={color} />
+            ) : (
+              <HomeIcon width={28} height={28} color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: "Setting",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <SettingIconFocused width={44} height={44} color={color} />
+            ) : (
+              <SettingIcon width={28} height={28} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <ProfileIconFocused width={44} height={44} color={color} />
+            ) : (
+              <ProfileIcon width={28} height={28} color={color} />
+            ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
---
+++ frontend/app/(tabs)/reminder.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ReminderScreen() {
+  return (
+    <View>
+      <Text>Reminder</Text>
+    </View>
+  );
+}
