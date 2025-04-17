46c63e642bea8b11bc79f09f150ca07eb8d9c5e4
bawfng04 <nguyendinhbang53az@gmail.com>
2025-04-17 17:03:33 +0700

    add Reminder APIs

--- backend/src/utils/schema.sql
+++ backend/src/utils/schema.sql
@@ -19,3 +19,23 @@ CREATE TABLE threshold (
   upper_value VARCHAR(255) NOT NULL,
   lower_value VARCHAR(255) NOT NULL
 );
+
+
+--Bảng reminders
+CREATE TABLE reminders (
+    id SERIAL PRIMARY KEY,
+    index_name VARCHAR(50) NOT NULL, -- 'temperature', 'humidity'...
+    higher_than_value NUMERIC,
+    higher_than_status BOOLEAN DEFAULT FALSE,
+    lower_than_value NUMERIC,
+    lower_than_status BOOLEAN DEFAULT FALSE,
+    repeat_after_value INTEGER,
+    repeat_after_status BOOLEAN DEFAULT FALSE,
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO reminders (index_name, higher_than_value, higher_than_status, lower_than_value, lower_than_status, repeat_after_value, repeat_after_status, active) VALUES
+('temperature', 40, TRUE, 19, TRUE, 24, TRUE, TRUE),
+('humidity', NULL, FALSE, 19, TRUE, NULL, FALSE, FALSE);
