eef94c761734a9b51ebba56ea4b1ffe61b5fb108
bawfng04 <nguyendinhbang53az@gmail.com>
2025-05-30 22:24:50 +0700

    add local db

--- backend/src/database/PostgreDatabase.js
+++ backend/src/database/PostgreDatabase.js
@@ -20,7 +20,19 @@ const poolConfig = connectionString
         rejectUnauthorized: false, //chỉ dùng khi connect bên ngoài
       },
     };
-
 const pool = new Pool(poolConfig);
 
+
+// local local local
+// const pool = new Pool({
+//   user: process.env.POSTGRES_USER,
+//   host: process.env.POSTGRES_HOST,
+//   database: process.env.POSTGRES_DB,
+//   password: process.env.POSTGRES_PASSWORD,
+//   port: process.env.POSTGRES_PORT,
+//   ssl: false,
+// });
+
 module.exports = { pool };
+
+
